a8fb625ad11a929858ae9da7ed3b411f9db4b45f
Edvins Antonovs <edvins.antonovs@british-gymnastics.org>
2016-08-18 15:19:35 +0100

    Added a temporary proteperty

--- app/index.js
+++ app/index.js
@@ -4,12 +4,12 @@ var ReactDOM = require('react-dom');
 var HelloWorld = React.createClass({
     render: function() {
         return (
-            <div> Hello React.js! </div>
+            <div> Hello {this.props.name}! </div>
         )
     }
 });
 
 ReactDOM.render(
-    <HelloWorld />,
+    <HelloWorld name="Edvins" />,
     document.getElementById('app')
 )
