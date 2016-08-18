22f0f07ec5d27e119b1e2f194c6ed0bdb95ff432
Edvins Antonovs <edvins.antonovs@british-gymnastics.org>
2016-08-18 16:07:38 +0100

    Avatar component is now completed

--- app/index.js
+++ app/index.js
@@ -1,15 +1,51 @@
+var USER_DATA = {
+    name: 'Edvins Antonovs',
+    username: 'ummahusla',
+    image: 'https://avatars1.githubusercontent.com/u/2536096?v=3&s=460'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
+    render: function() {
+        return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+    }
+});
+
+var ProfileLink = React.createClass({
     render: function() {
         return (
-            <div> Hello {this.props.name}! </div>
+            <div>
+                <a href={'http://www.github.com/' + this.props.username}>
+                    {this.props.username}
+                </a>
+            </div>
         )
     }
 });
 
+var ProfileName = React.createClass({
+    render: function() {
+        return (
+            <div>{this.props.name}</div>
+        )
+    }
+});
+
+var Avatar = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <ProfilePic imageUrl={this.props.user.image} />
+                <ProfileName name={this.props.user.name} />
+                <ProfileLink username={this.props.user.username} />
+            </div>
+        )
+    }
+})
+
 ReactDOM.render(
-    <HelloWorld name="Edvins" />,
+    <Avatar user={USER_DATA} />,
     document.getElementById('app')
 )
