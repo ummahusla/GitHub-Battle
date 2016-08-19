295e51b27a4e5d21f9c62cb3cb7bcba65ba97973
Edvins Antonovs <edvins.antonovs@british-gymnastics.org>
2016-08-19 11:41:10 +0100

    Added router config, and two components

---
+++ app/components/Main.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Main = React.createClass({
+    render: function() {
+        return (
+            <div> Hello from Main! </div>
+        )
+    }
+});
+
+module.exports = Main;
