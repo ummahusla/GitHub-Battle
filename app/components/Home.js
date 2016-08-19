0c575a028f557272f2f78fdd9162eb5fb947fda7
Edvins Antonovs <edvins.antonovs@british-gymnastics.org>
2016-08-19 13:52:36 +0100

    Implemented the first routing

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,7 +1,7 @@
 var React = require('react');
 
 var Home = React.createClass({
-    render: function() {
+    render: function () {
         return (
             <div> Hello from Home! </div>
         )
