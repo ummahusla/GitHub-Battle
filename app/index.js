e817185d2246f52486dc77a20917abfe7d10b97d
Edvins Antonovs <edvins.antonovs@british-gymnastics.org>
2016-08-19 15:21:21 +0100

    Added Sentry for error logging

--- app/index.js
+++ app/index.js
@@ -1,5 +1,23 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var routes = require('./config/routes');
+var Raven = require('raven-js');
+
+var sentryKey = '805bb6b2e41a4f85854e7d75dae99ea5';
+var sentryApp = '93121';
+var sentryURL = 'https://' + sentryKey + '@app.getsentry.com/' + sentryApp;
+
+var _APP_INFO = {
+  name: 'GitHub Battle',
+  branch: 'master',
+  version: '1.0'
+}
+
+Raven.config(sentryURL, {
+    release: _APP_INFO.version,
+    tags: {
+        branch: _APP_INFO.branch,
+    }
+}).install();
 
 ReactDOM.render(routes, document.getElementById('app'));
